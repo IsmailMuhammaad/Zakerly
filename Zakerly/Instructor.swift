edcd1678174d9c41f91f637672dde80f5f2b1389
Ismail Muhammad <Izma3iin@Gmail.com>
2021-06-26 03:57:00 +0200

    Scroll View

---
+++ Zakerly/Instructor.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Zakerly
+//
+//  Created by Izma3iin on 6/25/21.
+//
+
+import Foundation
+
+public struct Instructor {
+    let name : String
+    let speciality : String
+    let description : String
+    let image: UIImage
+    
+    
+    
+}
